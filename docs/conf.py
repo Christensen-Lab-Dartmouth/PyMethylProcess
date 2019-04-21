ada348c8adcbe27b5219450ff3ae4166bae6f880
jlevy44 <joshualevy44@berkeley.edu>
2019-04-21 17:54:39 -0400

    Small change to docs on future doc build. This will make docs more readable.

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
-    'sphinx_click.ext'
+    'sphinx_click.ext',
+    'numpydoc'
 ]
 html_theme = 'sphinx_rtd_theme'
 
